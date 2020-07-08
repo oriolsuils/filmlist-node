5b93e0b25144088922c58da7d1b54facb0b4742d
Oriol Suils <oriolsuils@gmail.com>
2020-07-08 19:58:11 +0200

    First dependencies for starting the server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.DB_CONNECTION, 
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+    () => { console.log("connected to db!"); }
+);
+
+app.listen(3000, () => console.log("Server up and running"));
